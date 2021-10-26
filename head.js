39d9284ab231a3e835779a0cf3e6d1c8873a9104
Jonathan Su <jsu6898@gmail.com>
2021-10-25 17:51:26 -0700

    looking for head

---
+++ head.js
@@ -0,0 +1,18 @@
+
+const head = function (toHead) {
+  return (toHead[0]);
+};
+
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🤑🤑🤑 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5);
