6a6c69900f4c647fa3c238a84a3708ea1db9a246
Jonathan Su <jsu6898@gmail.com>
2021-10-28 17:41:38 -0700

    take me home country roads to the place i belong

--- takeUntil.js
+++ takeUntil.js
@@ -20,3 +20,34 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+
+
+const eqArrays = function (arr1, arr2) {
+  let trueOrFalse = true;
+  if (arr1.length !== arr2.length) {
+    trueOrFalse = false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        trueOrFalse = trueOrFalse && true;
+      } else if (arr1[i] !== arr2[i]) {
+        trueOrFalse = trueOrFalse && false;
+      }
+    }
+  }
+  return trueOrFalse;
+};
+
+const assertArraysEqual = function (a1, a2) {
+  if (eqArrays(a1, a2)) {
+    console.log(`🤑🤑🤑 Assertion Passed: ${a1} === ${a2}`);
+  } else if (!eqArrays(a1, a2)) {
+    console.log(`🤬🤬🤬 Assertion Failed: ${a1} !== ${a2}`);
+  }
+};
+
+
+
+assertArraysEqual(data1, results1);
+assertArraysEqual(data2, results2);
