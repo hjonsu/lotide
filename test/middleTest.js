0eeead3627367b41ce0043a97749bb85e59d489b
Jonathan Su <jsu6898@gmail.com>
2021-11-01 15:31:52 -0700

    added mocha and chai functionality

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,6 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
 
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
 const test = [1, 2, 3, 4];
 const testOdd = [1, 2, 3, 4, 5];
